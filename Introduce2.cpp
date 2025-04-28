98c39f875562a09e87c45640b2cce72ed9828c3e
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-04-28 19:14:14 +0700

    membuat luas dari persegi panjang

--- Introduce2.cpp
+++ Introduce2.cpp
@@ -7,20 +7,27 @@ class PersegiPanjang
     float panjang;
     float luas;
     float lebar;
-
-    void displaydata() {
+    
+    void inputdata(){
         cout << " input panjang : ";
         cin >> panjang;
         cout << "input luas : ";
         cin >> luas;
-        
     }
+
     void menghitungLuas(){
         luas = panjang*lebar;
+        
+    }
+    void output(){
         cout << " luas persegi panjang = " << luas << endl;
     }
 };
 
 int main(){
     PersegiPanjang per ;
+    per.inputdata();
+    per.menghitungLuas();
+    per.output();
+    return 0;
 }
