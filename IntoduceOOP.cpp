30f30c0a96e8d1ff40571430ee7238ab3fa64bfb
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-04-28 18:16:10 +0700

    membuat nama member planet

---
+++ IntoduceOOP.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Planet{
+    private:
+        float gravitasi;
+    public:
+        string nama;
+        float diameter;
+
+        
+};
+int main()
+{
+
+}
